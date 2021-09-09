93f4e9003432f151233c68a35232ffd6dc338324
Colin Platt <colingplatt@gmail.com>
2021-09-09 10:31:06 +0200

    updating deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,5 @@
 const { ethers } = require("hardhat");
+//const { hre } = require("@nomiclabs/hardhat-etherscan");
 
 async function main() {
     const [deployer] = await ethers.getSigners();
@@ -7,11 +8,39 @@ async function main() {
   
     console.log("Account balance:", (await deployer.getBalance()).toString());
     
-    // We get the contract to deploy   
+    // We get the contracts to deploy 
+    // xLoot first
     const xLootContract = await ethers.getContractFactory("extensionLoot");
     const xLoot = await xLootContract.deploy();
   
-    console.log("xLoot deployed to:", xLoot.address);
+    console.log("xLoot deployed to: ", xLoot.address);
+
+    // now OG Loot
+    const LootContract = await ethers.getContractFactory("Loot");
+    const Loot = await LootContract.deploy();
+  
+    console.log("Loot deployed to: ", Loot.address);
+
+    // then do Platinum
+    const platTokenContract = await ethers.getContractFactory("PlatinumToken");
+    const Plat = await platTokenContract.deploy();
+  
+    console.log("$PLAT deployed to: ", Plat.address);
+
+    // Then deploy PlatDistributor
+    const platDistributorContract = await ethers.getContractFactory("PlatinumDistributor");
+    const distributor = await platDistributorContract.deploy(Plat.address);
+  
+    console.log("PlatDistributor deployed to: ", distributor.address);
+
+    // And now we transfer ownership of $PLAT to the distributor
+    await Plat.transferOwnership(distributor.address);
+
+    console.log("Ownership of $PLAT: ", await Plat.owner());
+
+    // Finally we log xLoot as as a distributable
+    await distributor.addDistributableContract(xLoot.address, 8001, 16000, "10000000000000000000000", false);
+
   }
   
   main()
